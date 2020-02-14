fea381fb952729defdd654fe9e4e50ecc8a81000
Malena Rey <malerey@male.5d640511d691.sn.mynetname.net>
2020-02-14 13:26:33 -0300

    fist commit

---
+++ main.js
@@ -0,0 +1,21 @@
+
+// HTTP --> protocolo
+// --> GET buscar informacion
+// --> POST crear informacion
+// --> PUT actualiza informacion
+// --> DELETE borra informacion
+
+// los metodos reciben PARAMETROS entre parentesis, si es mas de uno, separados por coma
+
+// fetch recibe UN parametro obligatorio: la direccion web
+// parametro opcional: UN OBJETO
+
+// fetch(`https://meli-fnsefibufx.now.sh/user`, {
+//   method: 'POST',
+//   headers: { 'Content-Type': 'application/json' },
+//   body: JSON.stringify(this.state.data)
+// })
+//   .then(data => data.json()
+//   ).then(result => {
+//    console.log(data)
+//   });
