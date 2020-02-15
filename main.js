562a95b663017507549283e58d4bad931ec21b87
Malena Rey <malerey@male.5d640511d691.sn.mynetname.net>
2020-02-14 21:48:35 -0300

    update con fetch dentro de otro fetch

--- main.js
+++ main.js
@@ -8,14 +8,41 @@
 // los metodos reciben PARAMETROS entre parentesis, si es mas de uno, separados por coma
 
 // fetch recibe UN parametro obligatorio: la direccion web
-// parametro opcional: UN OBJETO
+// parametro opcional: UN OBJETO que contenga:
+    // headers: especifican informacion sobre el fetch
+    // body: la informacion que vamos a enviar, generalmente el JSON.stringify de un objeto de javascript
 
-// fetch(`https://meli-fnsefibufx.now.sh/user`, {
-//   method: 'POST',
-//   headers: { 'Content-Type': 'application/json' },
-//   body: JSON.stringify(this.state.data)
-// })
-//   .then(data => data.json()
-//   ).then(result => {
-//    console.log(data)
-//   });
+
+
+    // Ultima parte de la clase: un fetch adentro de otro 
+
+
+    
+    fetch(`http://api.openweathermap.org/data/2.5/weather?q=${buscarCiudad}&units=metric&lang=es&appid=9f39cdba4d4a89d545134347942f077d`)
+    .then(res => res.json())
+    .then(datosClimaActual => {
+      fetch(`http://api.openweathermap.org/data/2.5/forecast?q=${buscarCiudad}&units=metric&lang=es&appid=9f39cdba4d4a89d545134347942f077d`)
+      .then(res => res.json())
+      .then(datosPorHora => {
+      
+    // solamente aqui dentro tengo acceso a datosClimaActual y datosPorHora a la vez. 
+    // Si quisiera maquetar una tarjeta usando ambos datos, deberia hacerlo aqui
+
+    })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
